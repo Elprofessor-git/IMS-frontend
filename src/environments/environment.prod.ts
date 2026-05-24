69267e9fd79e84b6999a0a2774c655ac67a6a299
Elprofessor-git <boubaker.dev@zohomail.com>
2026-05-24 19:57:15 +0100

    config: apiUrl Render production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ims-backend-production-cafa.up.railway.app/api',
+  apiUrl: 'https://ims-backend-g95v.onrender.com/api',
   apiTimeout: 30000,
   tokenKey: 'textile_auth_token',
   refreshTokenKey: 'textile_refresh_token'
